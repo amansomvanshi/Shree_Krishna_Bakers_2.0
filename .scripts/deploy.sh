20d1da3bf6798a013537f4e564193bfa799388e6
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2026-04-16 08:07:45 +0530

    script added - 8

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,18 +5,19 @@ echo "Deployment started ... ⏳⏳"
 
 cd ~/
 
-#copy the .env files
+# copy the .env files
 cp ~/frontend.env ~/Shree_Krishna_Bakers_2.0/frontend/.env
 cp ~/backend.env ~/Shree_Krishna_Bakers_2.0/backend/.env
 
-# Pull the latest version of the app
+# go into repo FIRST
+cd ~/Shree_Krishna_Bakers_2.0
+
+# Pull latest code
 git pull origin main
 echo "Pulled successfully 👌"
 
-cd ~/Shree_Krishna_Bakers_2.0
-
 echo "going for frontend"
-cd ./frontend
+cd frontend
 
 echo "Installing Frontend Dependencies... ⏳"
 npm install --yes
@@ -26,8 +27,6 @@ echo "Building Frontend... ⏳"
 npm run build
 echo "Frontend Built 👌"
 
-#
-
 echo "going for backend"
 cd ../backend
 
@@ -35,10 +34,6 @@ echo "Installing Backend Dependencies... ⏳"
 npm install --yes
 echo "Backend Dependencies Installed 👌"
 
-
-
 echo "Server Starting... ✨✨✨"
 pm2 restart server || pm2 start server.js --name server
 pm2 save
-
-# git update-index --add --chmod=+x deploy.sh
